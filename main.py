3a79776b9c8eed7c878b6066f38698001e13c65f
Bibek Pradhan <134042662+ADD2LIST@users.noreply.github.com>
2023-07-05 10:06:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,29 @@
 import streamlit as st
-import openai
-import os
+import io
+import requests
+import json
 
-# Retrieve API key from environment variable
-api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = api_key
+# Get the OpenAI API key from Streamlit secrets
+api_key = st.secrets["openai_api_key"]
 
-def main():
-    st.title("Audio Transcription with OpenAI")
-    
-    # Upload audio file
-    audio_file = st.file_uploader("Upload audio file", type=["mp3", "wav"])
-    
-    if audio_file is not None:
-        # Transcribe audio
-        st.text("Transcribing audio...")
-        transcript = transcribe_audio(audio_file)
-        
-        # Display the transcript
-        st.text_area("Transcript", transcript)
-    
+# Create the axios request
+url = "https://api.openai.com/v1/engines/whisper/completions"
+headers = {"Authorization": f"Bearer {api_key}"}
+data = {"prompt": "Transcribe this audio file:"}
+
+# Upload the audio file
+file_upload = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
+if file_upload is not None:
+    audio_file = file_upload.read()
+
+    # Send the axios request
+    response = requests.post(url, headers=headers, data=data, files={"file": audio_file})
+
+    # Check the response status code
+    if response.status_code == 200:
+        # Get the transcription
+        transcription = json.loads(response.content)["choices"][0]["text"]
+        st.write(transcription)
     else:
-        st.warning("Please upload an audio file.")
-
-def transcribe_audio(audio_file):
-    try:
-        # Read audio file content
-        audio_content = audio_file.read()
-        
-        # Call OpenAI API to transcribe the audio
-        response = openai.Audio.transcribe("whisper-1", audio_content)
-        
-        # Get the transcript
-        transcript = response['transcriptions'][0]['text']
-        
-        return transcript
-    
-    except Exception as e:
-        st.error(f"Error occurred: {str(e)}")
-
-if __name__ == "__main__":
-    main()
+        st.error("Error getting transcription")
 
